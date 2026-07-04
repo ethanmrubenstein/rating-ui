be7a3a11eb07b2858d8609451c66820f2f0e2cf0
Ethan Rubenstein <ethanmrubenstein@gmail.com>
2026-07-03 22:29:36 -0600

    Refactors Rating to use Button component

--- src/components/Rating.jsx
+++ src/components/Rating.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import Star from "./Star";
 import Modal from "./Modal";
+import Button from "./Button";
 
 const Rating = ({
   heading = "Rate Your Experience",
@@ -45,13 +46,14 @@ const Rating = ({
       </div>
       {rating > 0 && <p className="feedback">{feedbackMessages[rating - 1]}</p>}
 
-      <button
+      <Button
         className="submit-btn"
         onClick={handleSubmit}
         disabled={rating === 0}
       >
         Submit
-      </button>
+      </Button>
+
       <Modal isOpen={submitted} onClose={closeModal} rating={rating} />
     </div>
   );
